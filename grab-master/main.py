9a5f9d4b3d832bb8bbc81823b239f76baf930b9b
Marina Vinyes <vinyes.marina@gmail.com>
2016-12-19 16:49:41 +0100

    adding plot

--- grab-master/main.py
+++ grab-master/main.py
@@ -1,4 +1,5 @@
 import matplotlib
+import matplotlib.pylab as plt
 
 matplotlib.use('Agg')
 
@@ -24,7 +25,10 @@ S = np.cov(data)
 print "Theta: ", Theta
 print "Overlappign Blocks: ", blocks
 
-
-
-
-
+matrix=S
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.set_aspect('equal')
+plt.imshow(Theta,interpolation='nearest', cmap=plt.cm.ocean)
+plt.colorbar()
+plt.show()

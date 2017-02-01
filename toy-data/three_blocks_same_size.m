d5772d5fa0ab9562762cf40c01d4085e42269353
vinyesm <vinyes.marina@gmail.com>
2017-02-01 15:46:30 +0100

    script with toy example

---
+++ toy-data/three_blocks_same_size.m
@@ -0,0 +1,83 @@
+% Toy example
+% with three blocks of size 5
+
+clear all; clc; close all;
+%% settings
+
+n=50; % number of samples
+
+pl=3; % number of latept variables
+po=15;% number of observed variables
+pt=po+pl;
+sigma2l=.8;
+sigma2lo=.5;
+
+% slo=.2; % level of sparsity on latent-observed block of conceptration mat
+% soo=.2; % level of sparsity on latent-observed block of conceptration mat
+Doo=eye(po)/sigma2lo;
+Dol=zeros(po,pl);
+Dol(1:5,1)=ones(5,1);
+Dol(6:10,2)=ones(5,1);
+Dol(11:15,3)=ones(5,1);
+Dol=-Dol/sigma2lo;
+Dll=eye(pl)*(1/sigma2l+5/sigma2lo);
+
+%% construction of the conceptration matrix
+Dfull=zeros(pt);
+Dfull(1:pl,1:pl)=Dll;
+Dfull((pl+1):pt,(pl+1):pt)=Doo;
+Dfull(1:pl,(pl+1):pt)=Dol';
+Dfull((pl+1):pt,1:pl)=Dol;
+
+Dfull=.5*(Dfull+Dfull');
+Dmargo=Doo-Dol*Dol';
+
+
+descr = {'Plot of the groundtruth '
+    'concentration matrix';
+    'of the complete model'};
+
+figure(1);clf;
+subplot(1,2,1)
+axis off;
+text(0,.5,descr)
+pbaspect([1 1 1]);
+subplot(1,2,2)
+imagesc(abs(Dfull));
+colorbar;
+pbaspect([1 1 1]);
+title('true complete conc. mat.');
+
+if eigs(Dfull,1,'sa')<0
+    error('The conceptration matrix of the complete model is not PSD \n')
+end
+
+%% sampling data
+mu=zeros(1,pt); % vector of means
+Xfull=mvnrnd(mu, inv(Dfull), n)';
+X=Xfull((pl+1):pt,:);
+S=cov(X');
+% S=inv(Dmargo);
+
+%% plotting
+
+figure(2);clf;
+subplot(2,2,1);
+imagesc(abs(Dmargo));
+pbaspect([1 1 1]);
+title('true marginal conc. mat.');
+subplot(2,2,2);
+imagesc(abs(inv(S)));
+pbaspect([1 1 1]);
+title('observed conc. mat.');
+subplot(2,2,3);
+imagesc(abs(inv(Dmargo)));
+pbaspect([1 1 1]);
+title('true cov');
+subplot(2,2,4);
+imagesc(abs(S));
+pbaspect([1 1 1]);
+title('observed cov');
+
+
+ 

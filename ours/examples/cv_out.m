1d3e89a456b868886b634f94473c2407a8568bbc
Marina Vinyes <vinyes.marina@gmail.com>
2017-03-20 11:39:46 +0100

    creating cross validation grid function

---
+++ ours/examples/cv_out.m
@@ -0,0 +1,24 @@
+function [cvgrid, pair_best, mincv]= cv_out(cv_cell,nb_part,pair,las,jcut)
+
+cv=zeros(nb_part,length(pair));
+for j=1:nb_part
+    for jj=1:length(pair)
+        cv(j,jj)=cv_cell{j}{jj};
+    end
+end
+%%
+mcv=mean(cv);
+cvgrid=inf*ones(length(las));
+count=1;
+mincv=inf;
+pair_best=0;
+for i=1:length(las)
+    for j=max(1,i-jcut):i
+        cvgrid(i,j)=mcv(count);
+        if mcv(count)<mincv
+            mincv=mcv(count);
+            pair_best=count;
+        end
+        count=count+1;
+    end
+end

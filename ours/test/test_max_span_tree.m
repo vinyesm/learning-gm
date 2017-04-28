ad1c51718038a15984b8850ecc9abf3293748e8c
Marina Vinyes <vinyes.marina@gmail.com>
2017-04-28 16:11:30 +0200

    adding spanning tree function to create sparse pattern on observed varibles in examples

---
+++ ours/test/test_max_span_tree.m
@@ -0,0 +1,10 @@
+%ControlCentor
+addpath ../utils/
+
+% CostMatrix can be considered as the cost matrix
+CostMatrix = rand(30);
+
+% Tree is the matrix saving the tree, and Cost is summation of all cost
+% upon arcs.
+[ Tree,Cost ] =  UndirectedMaximumSpanningTree ( CostMatrix )
+h1 = view(biograph( Tree ))

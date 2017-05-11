fa125b43ab3208a0805dcfc212e58075e1740521
vinyesm <vinyes.marina@gmail.com>
2017-05-11 18:36:47 +0200

    desc

--- ours/examples/pp_movielens.m
+++ ours/examples/pp_movielens.m
@@ -1,3 +1,18 @@
+% Movielens data.
+% Using the Movielens movie rating data set, we choose the rating  scores 
+% given  by  the  most  active  600  users  and  for  the  highest  rated  
+% 20 movies from each of the following three genres:
+% Horror,Children’s, andAction.
+% 
+% This results in a 600×60rating matrix with 56percent completeness. 
+% We consider the joint distribution of 60 movie rating variables as 
+% a LVGGM with three latent variables.  Each user’s rating vector is 
+% treated as an i.i.d. sample from the LVGGM. Since the true covariance 
+% matrix is unknown, we use the sample covariance matrix
+% as a proxy (as n p). Each covariance element is weighted by the actual number
+% of observations to compensate for the missingness in the data.
+
+
 addpath ../../ml-100k/
 
 R=importdata('u.data');

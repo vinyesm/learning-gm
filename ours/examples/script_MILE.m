4b5412870eedb9c5111daab5759df025391e9d13
vinyesm <vinyes.marina@gmail.com>
2017-05-17 21:34:25 +0200

    MILE

---
+++ ours/examples/script_MILE.m
@@ -0,0 +1,42 @@
+clear all;
+addpath('../main');
+addpath('../active-set');
+addpath('../atom-selection');
+addpath('../utils');
+% addpath('../other');
+addpath('../prox');
+addpath('../TPower_1.0');
+addpath('../TPower_1.0/algorithms/TPower/');
+addpath('../TPower_1.0/misc/');
+
+run('pp_MILE.m');
+
+p=size(S,1);
+
+%%
+
+
+
+las=[1 .1 .05];
+mus=[.01 .05 .01];
+
+for lam=las
+    for mu=mus
+        p=size(S,1);
+        param.f=4;
+        param.verbose=1;
+        inputData.X1=S^.5;
+        inputData.X2=S^.5;
+        inputData.Y=-eye(p);
+        
+
+        param.cardfun=inf*ones(1,p);
+        param.cardfun(100)=1;
+
+        param.lambda=lam;
+        param.mu=mu;
+        param.max_nb_main_loop=100;
+        [Z Z1 Z2 ActiveSet hist param flag output] = cgan_l1_omega(inputData,param);
+        save(['MILE_100_lam_' num2str(lam) '_rank_' num2str(ActiveSet.atom_count)], 'Z','Z1','Z2','ActiveSet','param');
+    end
+end

bb47c4f36333d5a65b7719d591ec7491e109573d
vinyesm <vinyes.marina@gmail.com>
2017-06-15 18:12:32 +0200

    updates

--- ours/utils/update_sparse.m
+++ ours/utils/update_sparse.m
@@ -1,3 +1,15 @@
 function S = update_sparse(param,L,S,D)
 
+switch param.f
+%     case 1 % prox
+%         H = Z - inputData.Y;
+
+%     case 4 % bilinear
+%         H = inputData.X1'*(inputData.X1*Z*inputData.X2 - inputData.Y)*inputData.X2';
+%     case 5 % score matching 
+% %         H = .5*(inputData.X*Z+Z*inputData.X)-inputData.Y;
+%         H = .5*(inputData.X*Z+Z*inputData.X)-inputData.Y;
+end
+
+
 end

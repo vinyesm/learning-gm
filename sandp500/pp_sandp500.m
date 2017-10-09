5c81cb591ab270b351539c7342f6b269d176d99e
vinyesm <vinyes.marina@gmail.com>
2017-10-09 14:24:52 +0200

    adding industry class

--- sandp500/pp_sandp500.m
+++ sandp500/pp_sandp500.m
@@ -1,20 +1,38 @@
-close all;
+close all;clear all; clc;
 addpath individual_stocks_5yr
 
 listing = dir('individual_stocks_5yr');
 
 X = [];
+industry = [];
 names = {};
 
+keySet =   {'Consumer Discretionary', 'Consumer Staples','Utilities' ...
+            'Energy','Industrials', 'Materials', ...
+            'Real Estate', 'Financials',...
+            'Information Technology', 'Telecommunication Services',...
+            'Health Care'};
+valueSet = 1:length(keySet);
+mapObj = containers.Map(keySet,valueSet);
+
+%%
+TI = readtable('industries.csv');
+%%
 for id=1:500%length(listing)
     %A = csvread(listing(3).name); 
     T = readtable(listing(id+3).name);
 %     keyboard
+    
     if length(T.Close)==1258
+        aaa = listing(id+3).name;
+        nn = strsplit(aaa,'_');
+        nn =nn(1);
+        ind=find(ismember(TI.Code,nn));
+         if ~isempty(ind)
          X = [X;T.Close'];
-         aaa = listing(id+3).name;
-         nn = strsplit(aaa,'_');
-         names = [names nn(1)];
+         names = [names nn];
+         industry = [industry TI.Industry(ind)];
+         end
     end
 %     X(id,:)=T.Close;
 end
@@ -50,7 +68,7 @@ imagesc(Sigma(I,I)); colormap jet;
 
 save('sandp500','Sigma','names', 'I');
 
-%%
+%% remove principal component
 [V,D]= eig(Sigma);
 vi=V(I,1);
 Sigma2 = Sigma(I,I)-D(1,1)*(vi*vi');
@@ -61,7 +79,46 @@ I2 = optimalleaforder(Z,pdist(Sigma2));
 figure(3);clf;
 imagesc(abs(Sigma2(I2,I2))); colormap jet;
 
-Sigma=0.5*(Sigma2+Sigma2');
-I=I2;
-save('sandp500','Sigma','names', 'I');
+%% Per industry ordering
+
+indvalues = zeros(1,length(industry));
+for i=1:length(industry)
+    indvalues(i)=mapObj(industry{i});
+end
+[~, K]=sort(indvalues);
+
+figure(4);clf;
+subplot(1,2,1)
+imagesc(abs(Sigma(K,K))); colormap jet;
+subplot(1,2,2)
+imagesc(indvalues(K)'); colormap jet;
+pbaspect([50 length(industry) 1])
+
+figure(5);clf;
+subplot(1,2,1)
+imagesc(abs(Sigma2(K,K))); colormap jet;
+subplot(1,2,2)
+imagesc(indvalues(K)'); colormap jet;
+pbaspect([50 length(industry) 1])
+
+figure(6); clf;
+subplot(1,2,1)
+imagesc(abs(Sigma2(I2,I2))); colormap jet;
+subplot(1,2,2)
+imagesc(indvalues(I2)'); colormap jet;
+
+figure(7); clf;
+subplot(1,2,1)
+imagesc(abs(Sigma(I,I))); colormap jet;
+subplot(1,2,2)
+imagesc(indvalues(I)'); colormap jet;
+
+
+%%
+% Sigma=0.5*(Sigma2+Sigma2');
+% I=I2;
+% 
+save('sandp500','Sigma','names','industry', 'indvalues', 'I','K');
+
+
 
